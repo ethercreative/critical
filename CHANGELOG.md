aa3fb4a6f951ce3ef1d59dafaafcd84a4522c635
Tam <hi@tam.sx>
2018-10-18 13:05:07 +0100

    Basic plugin setup

---
+++ CHANGELOG.md
@@ -0,0 +1,1 @@
+## [Unreleased] 0.0.1
---
+++ src/Critical.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Let's Get Critical
+ *
+ * @link      https://ethercreative.co.uk
+ * @copyright Copyright (c) 2018 Ether Creative
+ */
+
+namespace ether\critical;
+
+use craft\base\Plugin;
+
+/**
+ * Class Critical
+ *
+ * @author  Ether Creative
+ * @package ether\critical
+ */
+class Critical extends Plugin
+{
+
+	// Properties
+	// =========================================================================
+
+	public $schemaVersion = '0.0.1';
+	public $hasCpSettings = false;
+	public $hasCpSection  = false;
+
+	// Plugin
+	// =========================================================================
+
+	public function init ()
+	{
+		parent::init();
+
+		//
+	}
+
+}
